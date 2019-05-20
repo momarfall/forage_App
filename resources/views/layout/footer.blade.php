5f52f2afc1d2635bcc1cc7120eb8cc30275c0753
dev <momarofall@gmail.com>
2019-05-20 18:44:54 +0000

    First Commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.0.0
+    </div>
+    <strong>Copyright &copy; 2019 <a href="#">Moustapha Diabang</a>.</strong> All rights
+    reserved.
+  </footer>
